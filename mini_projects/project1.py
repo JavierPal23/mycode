743c8ff9543654690e688da14742ce1a5b7ed3bc
JavierPal23 <javier.palacios2.jr@gmail.com>
2023-02-15 03:25:46 +0000

    first mini project

---
+++ mini_projects/project1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" TLG | Javier Palacios
+    What 'Drum and Bass' DJ you'll like based on your favorite color."""
+
+print("What is your name? ")
+user_name = input()
+
+print("Hello", user_name , "what is your favorite color? ")
+answer = input()
+answer = answer.lower()
+
+
+if answer == "yellow":
+    print("You picked", answer , "you'll more than likely like Subtronics")
+
+if answer == "green":
+    print("You picked", answer , "you'll more than likely like Level Up") 
+
+if answer == "blue":                                             print("You picked", answer , "you'll more than likely like Kai Wachi")
+
+if answer == "orange":
+    print("You picked", answer , "you'll more than likely like Illenium")
+
+if answer == "black":
+    print("You picked", answer , "you'll more than likely like Excision")
+
+if answer == "red":
+    print("You picked", answer , "you'll more than likely like Sullivan King")
+
+
+while answer not in ["yellow", "green", "blue", "orange", "black", "red"]:
+    print("Try another color")
+    break
+else:
+   print("You dont know who that is? Go listen to your mainstream music.")
